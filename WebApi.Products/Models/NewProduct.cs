b98dff8532fb25baf7251361528dde40699830c8
sjafru <afandy.cooz@gmail.com>
2022-08-02 21:45:19 +0700

    Enable Fluent Validation

---
+++ WebApi.Products/Models/NewProduct.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using Lib.Products.Domain;
+
+namespace WebApi.Products.Models;
+
+public record NewProduct(string Name, ProductType Type = ProductType.Standard)
+{
+    public decimal BasePrice { get; set; }
+    public string SKU { get; set; }
+}
+
+public class NewProductValidator : AbstractValidator<NewProduct>
+{
+    public NewProductValidator()
+    {
+        RuleFor(p => p.Name).NotEmpty();
+    }
+}
--- WebApi.Products/Program.cs
+++ WebApi.Products/Program.cs
@@ -1,12 +1,16 @@
+using FluentValidation;
+using Lib.Products.Domain;
 using Lib.Products.Entities;
 using Microsoft.EntityFrameworkCore;
+using WebApi.Products.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddDbContext<ProductsDbContext>(opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -28,27 +32,34 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/products", async (ProductsDbContext db) => {
+app.MapGet("/products", async (ProductsDbContext db) =>
+{
 
 }).WithName("GetProducts").ProducesValidationProblem(400).Produces(200);
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("/products", (NewProduct req, IValidator<NewProduct> validator) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var valresult = validator.Validate(req);
+    if(!valresult.IsValid)
+        return Results.ValidationProblem(valresult.ToDictionary());
+
+    var product = new ProductDetail();
+
+    return Results.Created($"/{product.ID}", product);  
+}).WithName("AddProduct").ProducesValidationProblem(400).Produces(200);
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast = Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateTime.Now.AddDays(index),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
